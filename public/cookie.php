a6d9e6e6ce75e6acbbfcc684df98450b4232b8e9
Marco Ziech <marco@ziech.net>
2020-01-09 00:17:30 +0100

    Add license

--- public/cookie.php
+++ public/cookie.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * divera-spreadsheet - A tool to format Divera API responses as a spreadsheet
+ * Copyright © 2020 Marco Ziech (marco@ziech.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 include __DIR__. '/../vendor/autoload.php';
 if ($_POST['action'] === 'set') {
     \DiveraSpreadSheet\Authentication::setDashboardCookie($_POST['token']);
